9e2d9d9d4e10e7493f8f905df82e016589f0688f
jwcho95 <kyaleee@naver.com>
2019-12-08 17:03:08 +0900

    최종수정날짜: 2019/12/08

--- chapter 3/chapter 3.c
+++ chapter 3/chapter 3.c
@@ -1,7 +1,7 @@
 // <혼자 공부하는 C언어> 챕터 3-1
 
 // 변수의 선언과 사용
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main(void)
 {
@@ -21,6 +21,20 @@ int main(void)
 	printf("변수 c의 값 : %d\n", c);
 	printf("변수 da의 값 : %.1lf\n", da);
 	printf("변수 ch의 값 : %c\n", ch);
+
+	return 0;
+} */
+
+// char형 변수의 사용
+#include <stdio.h>
+
+int main(void)
+{
+	char ch1 = 'A'; // char 형은 문자를 주로 저장하는 용도로 사용된다.
+	char ch2 = 65; // 컴파일러는 0 ~ 127 사이의 정수(아스키 코드 값)으로 바꾸어 처리하기에 char형을 사용하면 문자를 효과적으로 저장할 수 있다.
+
+	printf("문자 %c의 아스키 코드 값 : %d\n", ch1, ch1);
+	printf("아스키 코드 값이 %d인 문자 : %c\n", ch2, ch2);
 
 	return 0;
 }
