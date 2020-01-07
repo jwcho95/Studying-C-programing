89138e80f1d0fd467d6713534facdb6285f8f5b3
jwcho95 <kyaleee@naver.com>
2020-01-07 23:10:55 +0900

    최종수정날짜: 2020/01/07

--- 모두의 C언어 (3장)/Chapter 3.c
+++ 모두의 C언어 (3장)/Chapter 3.c
@@ -53,7 +53,7 @@ int func10(int x)
 
 
 // 값을 입력 후 더하기 계산하기
-#define _CRT_SECURE_NO_WARNINGS // 이 문장이 없으면 비쥬얼 스튜디오에서는 보안상의 이유로 아래의 코드가 실행되지 않는다.
+/* #define _CRT_SECURE_NO_WARNINGS // 이 문장이 없으면 비쥬얼 스튜디오에서는 보안상의 이유로 아래의 코드가 실행되지 않는다.
 #include <stdio.h>
 
 double abc(int x, int y);
@@ -76,4 +76,24 @@ main()
 double abc(int x, int y)
 {
 	return x + y + 3.14;
+} */
+
+
+// 들어오고 나가는 게 없을 때 사용하는 void
+#include <stdio.h>
+
+int test1(void); // void의 뜻은 '공허한', '빈' 이라는 뜻을 지니고 있으며 C언어에서는 함수의 입력 또는 출력이 없을 때 void를 쓰거나 아무것도 안 쓴다.
+				 // int test1(void)랑 int test1()이랑은 같은 의미라는 소리이다.
+
+main()
+{
+	int result;
+	result = test1();
+
+	printf("test1 함수로부터 돌려받은 값은 %d\n", result);
+}
+
+int test1(void) // 내가 항상 즐겨쓰는 int main(void)는 그저 main이란 함수의 본체였을 뿐이였다.
+{
+	return 10;
 }
