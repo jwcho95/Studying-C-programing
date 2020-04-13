5568304475646e5c9796a8fa2b11a2ad1541e764
jwcho95 <kyaleee@naver.com>
2020-04-13 18:24:44 +0900

    최종수정날짜: 2020/04/13

--- 모두의 C언어 (3장)/Chapter 3.c
+++ 모두의 C언어 (3장)/Chapter 3.c
@@ -80,7 +80,7 @@ double abc(int x, int y)
 
 
 // 들어오고 나가는 게 없을 때 사용하는 void
-#include <stdio.h>
+/*#include <stdio.h>
 
 int test1(void); // void의 뜻은 '공허한', '빈' 이라는 뜻을 지니고 있으며 C언어에서는 함수의 입력 또는 출력이 없을 때 void를 쓰거나 아무것도 안 쓴다.
 				 // int test1(void)랑 int test1()이랑은 같은 의미라는 소리이다.
@@ -96,4 +96,68 @@ main()
 int test1(void) // 내가 항상 즐겨쓰는 int main(void)는 그저 main이란 함수의 본체였을 뿐이였다.
 {
 	return 10;
+}*/
+
+
+// 출력이 없을 때도 사용하는 void
+/*#include <stdio.h>
+
+void test2(int x);
+
+main()
+{
+	test2(3); // 어떠한 값도 main 함수에 돌려주지 않았다.
+}
+
+void test2(int x)
+{
+	printf("메인 함수로부터 받은 값은 %d입니다.\n", x);
+}*/
+
+
+// 함수 연습
+#include <stdio.h>
+
+int func1(void);
+char func2(void);
+func3(void);
+func4(int x);
+
+main()
+{
+	int input_n;
+
+	func3();
+	printf("원하시는 숫자를 입력하세요:");
+	scanf_s("%d", &input_n); // 입력받은 값은 4번째 함수로 간다.
+	printf("입력하신 숫자는 %d이고, 첫번째 함수로부터 받은 값은 %d이고, 두번째 함수로부터 돌려받은 문자는 %c이다.\n", input_n, func1(), func2());
+	printf("그리고 네번째 함수는 입력한 숫자를 10배로 돌려주기에 %d을 돌려주었다.", func4(input_n));
+
+	return 0;
+}
+
+int func1() // 숫자 1을 반환하는 함수
+{
+	return 1;
+}
+
+char func2() // 문자 A를 반환하는 함수
+{
+	return 'A';
+}
+
+func3() // 호출하면 Hello를 출력하는 함수
+{
+	printf("Hello\n");
+}
+
+func4(int x) // 입력받은 수를 10배로 늘려 반환하는 함수
+{
+	int new_n, mul;
+
+	new_n = x;
+	
+	mul = new_n * 10;
+
+	return mul;
 }
